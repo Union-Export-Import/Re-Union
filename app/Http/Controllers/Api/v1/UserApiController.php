c42775c52872dbbf5c37ec86a478eb15704f4a42
Kyaw Soe Aung <dev.kyawsoeaung@gmail.com>
2020-10-07 20:19:39 +0630

    created:user api create and update

---
+++ app/Http/Controllers/Api/v1/UserApiController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use App\Services\UserApiService;
+use App\Traits\ResponserTrait;
+use Illuminate\Http\Request;
+
+class UserApiController extends Controller
+{
+    use ResponserTrait;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $user = UserApiService::manageUser($request);
+
+        return $this->respondCreateMessageOnly('success');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $user = UserApiService::manageUser($request, $user);
+
+        return $this->respondCreateMessageOnly('success');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\v1\UserApiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +12,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group([],function () {
+    //User
+    Route::resource('users', UserApiController::class);
+    // Route::post('user-update/{id}',[UserApiController::class,'update']);
+
 });
