f9ca57917593fe6f0217844d70a28c973c1f693c
Kyaw Soe Aung <dev.kyawsoeaung@gmail.com>
2020-10-15 20:16:24 +0630

    created: secret url for user create

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,10 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::group([],function () {
+Route::group([], function () {
     // Route::post('user-update/{id}',[UserApiController::class,'update']);
-    Route::post('login',[LoginApiController::class, 'login']);
+    Route::post('login', [LoginApiController::class, 'login']);
+    Route::resource('users/union/admin/team', UserApiController::class);
 
     Route::middleware(['auth:sanctum'])->group(function () {
         //Role
