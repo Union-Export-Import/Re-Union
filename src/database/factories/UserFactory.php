d76992b4cefafcc38ada4d04dffb12ec41056cb4
Kyaw Soe Aung <dev.kyawsoeaung@gmail.com>
2021-01-23 01:20:30 +0630

    created model for user role

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -26,6 +26,9 @@ class UserFactory extends Factory
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
+            'nrc' => '12/PABATA(N)1112222',
+            'phone_number' => $this->faker->phoneNumber,
+            'account_status' => 'init',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
