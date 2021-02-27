3f63b1584fb67b80ece324f5ff2f00d9d102d6cc
Kyaw Soe Aung <dev.kyawsoeaung@gmail.com>
2021-02-27 21:21:20 +0630

    change V1 to v1

--- app/Http/Controllers/Api/v1/AssetTypeApiController.php
+++ app/Http/Controllers/Api/v1/AssetTypeApiController.php
@@ -1,7 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\V1;
-
+namespace App\Http\Controllers\Api\v1;
 use App\Models\AssetType;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
